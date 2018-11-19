d63eb0b8d5d51dbd7867a14d3d9ae7b648ee48ac
Gustavo <gujtavitocr@gmail.com>
2018-11-18 21:07:42 -0400

    Counts words, missing order function.

--- WordCounter.go
+++ WordCounter.go
@@ -1,7 +1,49 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+	"unicode"
+	"io/ioutil"
+)
+
+
+func CountWords(text string) map[string]int {
+
+	freq := make(map[string]int)
+	for _, word := range strings.Fields(strings.ToLower(text)) {
+		word = strings.Map(func(r rune) rune {
+			if unicode.IsLetter(r) {
+				return r
+			} else {
+				return -1
+			}
+		}, word)
+		if word != "" {
+			freq[word] = freq[word] + 1
+		}
+		
+	}
+	
+	return freq
+
+}
 
 func main() {
-	fmt.Println("Hola Mundo")
+	
+	dataBin, err := ioutil.ReadFile("words.txt")
+	if err != nil {
+		fmt.Print(err)
+	}
+	textFile := string(dataBin)
+	
+	count := CountWords(textFile)
+
+	fmt.Printf("%+v", count)
+
+
+
+	
+	
+	
 }
